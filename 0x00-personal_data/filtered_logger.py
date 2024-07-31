9cdf3819b52006b22a8dc2054da037d54566ba0f
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-07-31 11:56:03 +0300

    creates the log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 Module for filtering and obfuscating log messages.
 """
 import re
+import logging
 from typing import List
 
 
@@ -33,3 +34,22 @@ def filter_datum(
         lambda m: m.group().split('=')[0] + '=' + redaction,
         message
     )
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        org_msg = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields,
+                            self.REDACTION,
+                            org_msg, self.SEPARATOR
+                            )
