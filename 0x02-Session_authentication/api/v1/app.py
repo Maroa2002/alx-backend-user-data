7bd53c9efd424be5dce911390509a28b0e5e449e
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-08-08 09:36:01 +0300

    checks for session cookie before the request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -45,15 +45,15 @@ def before_request():
     if auth.authorization_header(request) is None:
         abort(401)
 
-    if auth.current_user(request) is None:
-        abort(403)
-
     if (auth.authorization_header(request) is None
             and auth.session_cookie(request) is None):
-        return None
         abort(401)
 
-    request.current_user = auth.current_user(request)
+    current_user = auth.current_user(request)
+    if current_user is None:
+        abort(403)
+
+    request.current_user = current_user
 
 
 @app.errorhandler(404)
