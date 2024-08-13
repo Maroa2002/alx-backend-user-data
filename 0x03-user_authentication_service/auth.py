41b1fbf877480586ffce87482cff8e2b834f3807
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-08-13 13:32:53 +0300

    pycodestyle editing

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -11,13 +11,14 @@ def _hash_password(password: str):
     hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_pwd
 
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-    
+
     def register_user(self, email: str, password: str) -> User:
         """Implements user registration"""
         user = self._db.find_user_by(email=email)
@@ -25,8 +26,7 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         else:
             hashed_pwd = _hash_password(password)
-        
+
         new_user = self._db.add_user(email, hashed_pwd)
 
         return new_user
-
