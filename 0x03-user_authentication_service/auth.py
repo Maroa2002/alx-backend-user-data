0c2230b8730c7c8b5eb42ef6bd28b8e43cc12f90
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-08-14 12:30:13 +0300

    finds user from session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from typing import Union
 from uuid import uuid4
 
 
@@ -58,3 +59,15 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Finds user by session ID"""
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
+        return user
