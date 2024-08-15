de8746c33c157396fe3620fc150f81acb178792e
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-08-15 08:49:30 +0300

    updates passowrd

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -93,3 +93,14 @@ class Auth:
         self._db.update_user(user.id, reset_token=reset_token)
 
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Updates the password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError("User does not exist")
+
+        hashed_pwd = _hash_password(password)
+        user.hashed_password = hashed_pwd
+        self._db.update_user(user.id, hashed_password=hashed_pwd)
